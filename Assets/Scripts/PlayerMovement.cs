65be5009e3de0884b3641ab7e5c1925c8c72df18
sebaspapu <sebasnarutoxd@gmail.com>
2024-11-20 21:06:48 -0500

    feat: obstacles
    - se añaden objetos tipo obstaculos, se añaden los scripts que permiten la comunicación e interaccion del player cuando colisiona con el.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+
+    bool alive = true;
 
     public float speed = 5;
     public Rigidbody rb;
@@ -18,6 +21,8 @@ public class NewBehaviourScript : MonoBehaviour
     // movimiento a la esfera
     private void FixedUpdate()
     {
+        if (!alive) return;
+
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
@@ -28,5 +33,24 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        // para cuando se cae
+        if (transform.position.y < -5)
+        {
+            Die();
+        }
     }
+
+    public void Die()
+    {
+        alive = false;
+        // restaurar el juego con restraso de 2s
+        Invoke("Restart", 1);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
