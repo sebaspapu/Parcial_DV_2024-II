437289af2583a4ad6511abcef9b5c9af1b9f2686
sebaspapu <sebasnarutoxd@gmail.com>
2024-11-21 03:08:54 -0500

    feat: diferent type wall
    - se añadio un nuevo tipo de muro, con la idea de mejorar el diseño del escenario

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -6,6 +6,9 @@ public class GroundTile : MonoBehaviour
     public GameObject obstaclePrefab;
     public GameObject coinPrefab;
 
+    [SerializeField] GameObject tallObstaclePrefab;
+    [SerializeField] float tallObstacleChance = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,19 +29,28 @@ public class GroundTile : MonoBehaviour
 
     public void SpawnObstacle()
     {
+        // escoge que obstaculo va a crear
+        GameObject obstacleToSpawn = obstaclePrefab;
+        float random = Random.Range(0f, 1f);
+        if (random < tallObstacleChance)
+        {
+            obstacleToSpawn = tallObstaclePrefab;
+        }
+
+
         // Choose a random point to spawn the obstacle
         int obstacleSpawnIndex = Random.Range(2, 5);
         Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
 
         // Spawn the obstace at the position
-        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+        Instantiate(obstacleToSpawn, spawnPoint.position, Quaternion.identity, transform);
     }
 
 
     public void SpawnCoins()
     {
         // la cantidad de monedas que se van a generar en el escenario, por cada bloque
-        int coinsToSpawn = 3;
+        int coinsToSpawn = 2;
         for (int i = 0; i < coinsToSpawn; i++)
         {
             GameObject temp = Instantiate(coinPrefab, transform);
