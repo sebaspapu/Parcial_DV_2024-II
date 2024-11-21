24fad65e12b89781a1325c39efc1a4f8706c9ea2
sebaspapu <sebasnarutoxd@gmail.com>
2024-11-21 01:10:05 -0500

    feat: powerup and score
    - se crea el HUD del score que muestra la puntación al momento de que el jugador consigue los powerup.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int score;
+    public static GameManager inst;
+    public Text scoreText;
+    public PlayerMovement playerMovement;
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = "SCORE: " + score;
+        // Increase the player's speed
+        //playerMovement.speed += playerMovement.speedIncreasePerPoint;
+    }
+
+    private void Awake()
+    {
+        inst = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class GroundTile : MonoBehaviour
 {
     GroundSpawner groundSpawner;
+    public GameObject obstaclePrefab;
+    public GameObject coinPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
         SpawnObstacle();
+        SpawnCoins();
     }
 
     private void OnTriggerExit(Collider other)
@@ -23,8 +26,6 @@ public class GroundTile : MonoBehaviour
         
     }
 
-    public GameObject obstaclePrefab;
-
     public void SpawnObstacle()
     {
         // Choose a random point to spawn the obstacle
@@ -34,4 +35,35 @@ public class GroundTile : MonoBehaviour
         // Spawn the obstace at the position
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
     }
+
+
+    public void SpawnCoins()
+    {
+        // la cantidad de monedas que se van a generar en el escenario, por cada bloque
+        int coinsToSpawn = 3;
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            GameObject temp = Instantiate(coinPrefab, transform);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+
+    Vector3 GetRandomPointInCollider(Collider collider)
+    {
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        // con esto nos aseguramos de la altura
+        point.y = 0.5f;
+        return point;
+    }
+
+
 }
